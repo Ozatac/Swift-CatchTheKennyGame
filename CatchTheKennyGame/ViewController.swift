441c16cd0109a6c50e0739cf78c9adb9ba80d164
Tunahan Özataç <tunahan.ozatac@gmail.com>
2020-02-17 22:20:31 +0300

    Initial Commit

---
+++ CatchTheKennyGame/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CatchTheKennyGame
+//
+//  Created by Tunahan Özataç on 17.02.2020.
+//  Copyright © 2020 Tunahan Ozatac. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
